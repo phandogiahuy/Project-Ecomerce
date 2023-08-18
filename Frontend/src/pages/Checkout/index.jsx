f41fd21ba15ea14478faebf8edb3055aca30fed5
Huy <phandogiahuy2016@gmail.com>
2023-08-18 19:37:52 +0700

    h21

--- Frontend/src/pages/Checkout/index.jsx
+++ Frontend/src/pages/Checkout/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import Announcement from "../../components/Annoucement";
 import CheckoutComponent from "../../components/Checkout";
 import Footer from "../../components/Footer/Footer";
 
 const Checkout = () => {
   return (
     <div className="overflow-x-hidden">
-      <Announcement />
       <CheckoutComponent />
       <Footer />
     </div>
