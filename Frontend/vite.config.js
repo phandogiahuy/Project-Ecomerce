2577412f06090eea9b7e9cadff0bcaf10c0df862
Huy <phandogiahuy2016@gmail.com>
2023-08-24 18:16:02 +0700

    1234

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,26 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import path from "path";
+import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
+const aliases = {
+  "@crema": "src/@crema",
+  core: "src/core",
+  assets: "src/assets",
+  "@hook": "src/@hook",
+  components: "src/components",
+  features: "src/features",
+  guards: "src/guards",
+  pages: "src/pages",
+  types: "src/types",
+};
+
+const resolvedAliases = Object.fromEntries(
+  Object.entries(aliases).map(([key, value]) => [
+    key,
+    resolve(__dirname, value),
+  ])
+);
 export default defineConfig({
   server: {
     proxy: {
@@ -13,9 +31,19 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "react", // ignore react stuff
+        "react-dom",
+      ],
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      ...resolvedAliases,
+      "./runtimeConfig": "./runtimeConfig.browser",
+      "jss-plugin-{}": "jss-plugin-global",
     },
   },
 });
