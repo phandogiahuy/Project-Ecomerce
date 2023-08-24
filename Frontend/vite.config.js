29b4852e7481b2d6c1dbb3f5326cc735b75f51ec
Huy <phandogiahuy2016@gmail.com>
2023-08-24 18:05:30 +0700

    hello

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
 });
